5eb6a476ca601c7d4b9bf50c96500ecbff022a75
André Meneses <andre_meneses2001@hotmail.com>
2022-11-14 14:31:27 -0300

    Simplex melhorado, problema do transporte implementado

--- otimizacao.py
+++ otimizacao.py
@@ -8,7 +8,7 @@ def simplex(matriz,base,objetivo="max"):
     @param matriz: Matriz cuja primeira linha corresponde à função objetivo, e cujas linhas restantes
     correspondem às restrições do problema. 
     @param base: Lista de inteiros, representam a base inicial do problema. 
-    @param objetivo: string, indica que se a otimização é de maximização ou minimização
+    @param objetivo: string, indica se a otimização é de maximização ou minimização
 
     @returns matriz da solução e a base
 
@@ -29,7 +29,7 @@ def simplexMax(matriz, base):
     @returns matriz da solução e base
     """
 
-    funcaoObjetivo = np.array(matriz[0])
+    funcaoObjetivo = np.array(matriz[0][:-1])
     novaBase = np.argmin(funcaoObjetivo)
 
     matrizPasso = []
@@ -102,13 +102,55 @@ def gerarTabela(matriz,base):
 
     return pd.DataFrame(tabela)
 
+def matrizTransporte(quadroCusto,oferta,demanda):
+    matriz = []
+
+    # Função objetivo
+    matriz.append([1] + [quadroCusto[j][i] for i in range(len(demanda)) for j in range(len(oferta))] + [0])
+
+    #Equações de demanda
+    for i in range(len(demanda)):
+        mini = i*len(oferta)
+        maxi = mini + len(oferta)
+        matriz.append([0] + [1 if mini <= j < maxi else 0 for j in range(len(matriz[0])-2)] + [demanda[i]])        
+
+    # Equações de oferta
+    for i in range(len(oferta)):
+        matriz.append([0] + [1 if (j % len(oferta)) == i else 0 for j in range(len(matriz[0])-2)] + [oferta[i]])
+    
+    return matriz
+
+def formaCanonica(matriz,pivos):
+    novaMatriz = matriz
+
+    for i,j in pivos:
+        novaMatriz = eliminacaoPivotal(novaMatriz,i,j)
+
+    return novaMatriz
+
 if __name__ == '__main__':
-    a = [[1,-12,-15,0,0,0,0,0],[0,1,0,1,0,0,0,3],[0,0,1,0,1,0,0,4],[0,1,1,0,0,1,0,6],[0,1,3,0,0,0,1,13]]
+    # a = [[1,-12,-15,0,0,0,0,0],[0,1,0,1,0,0,0,3],[0,0,1,0,1,0,0,4],[0,1,1,0,0,1,0,6],[0,1,3,0,0,0,1,13]]
     #c = simplexMax(a,[3,4,5,6])
 
-    b = [[1, -2, -3, 0, 0, 0, 0],[0, 1, 0, 1, 0, 0, 3],[0, 0, 1, 0, 1, 0, 4],[0, 1, 3, 0, 0, 1, 12]]
-    d = simplexMax(b,[3,4,5])
+    # b = [[1, -2, -3, 0, 0, 0, 0],[0, 1, 0, 1, 0, 0, 3],[0, 0, 1, 0, 1, 0, 4],[0, 1, 3, 0, 0, 1, 12]]
+    # d = simplexMax(b,[3,4,5])
 
 
-    print(gerarTabela(d[0],d[1])) 
+    # print(gerarTabela(d[0],d[1])) 
+
+    demanda = [50,60,70,80]
+    oferta = [60,90,110]
+    quadroCusto = [[25,20,15,25],[15,20,500,10],[10,15,20,25]]
+    base = [1,4,5,8,9,12]
+
+    m = matrizTransporte(quadroCusto,oferta,demanda)
+    m = np.delete(m,4,axis=0)
+
+    pivos = [(1,1),(2,4),(5,5),(3,8),(6,9),(4,12)]
+    matrizSimplex = formaCanonica(m,pivos)
+
+    #print(gerarTabela(m,[1,4,5,8,9,12]))
+
+    res = simplexMax(matrizSimplex,base)
+    print(gerarTabela(res[0],res[1]))
 
